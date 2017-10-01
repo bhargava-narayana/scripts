0d407ec39f5e95b06b76a9ea0f42a136e8b55a5d
whois- <bhargava.naryana@gmail.com>
2017-10-01 00:57:46 -0700

    run in background

--- exec_vnc.sh
+++ exec_vnc.sh
@@ -1,4 +1,7 @@
 #/bin/sh
 
-x11vnc -rfbauth ~/.vnc/passwd  -forever -alwaysshared &
+#set password: x11vnc -storepasswd
+#view password: x11vnc -showrfbauth ~/.vnc/passwd
+
+x11vnc -rfbauth ~/.vnc/passwd  -forever -alwaysshared -bg -rfbport 5900
 
